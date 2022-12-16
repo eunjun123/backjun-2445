a4314be57ae79558a8a40d79bd831ba74f7fc44d
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 19:00:15 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class star
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            for (int k = 1; k <= (2 * n) - (2 * i); k++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        //아래
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write("*");
+            }
+            for (int k = 1; k <= 2 * i; k++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
